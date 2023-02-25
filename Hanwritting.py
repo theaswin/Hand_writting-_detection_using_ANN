17c6b56bfdba36363212536d2b3662220cdbe46d
aswinnath <aswinnathofficial@gmail.com>
2023-02-25 11:17:21 +0530

    added code

---
+++ Hanwritting.py
@@ -0,0 +1,26 @@
+import os
+import cv2
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import numpy as np
+
+mnist = tf.keras.datasets.mnist
+(x_train,y_train),(x_test,y_test) = mnist.load_data()
+
+x_train = tf.keras.utils.normalize(x_train,axis = 1)
+x_test = tf.keras.utils.normalize(x_test,axis = 1)
+
+model = tf.keras.models.Sequential()
+
+
+model.add(tf.keras.layers.Flatten(input_shape=(28,28)))
+model.add(tf.keras.layers.Dense(128, activation='relu'))
+model.add(tf.keras.layers.Dense(128,activation='relu'))
+model.add(tf.keras.layers.Dense(128,activation='softmax'))
+
+model.compile(optimizer = 'adam',loss = 'sparse_categorical_crossentropy',metrics=['accuracy'])
+
+
+model.fit(x_train,y_train,epochs=3)
+
+model.save('hand.model')
